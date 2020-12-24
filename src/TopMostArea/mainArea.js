42b079d9f2ed58349649cef7c2a9fd068d85181f
anurag55555 <anurag.walia@nineleaps.com>
2020-12-24 15:42:32 +0530

    [MainArea] | responsive | search box is made responsive along with search button and a handlechange and onclick events are added

--- src/TopMostArea/mainArea.js
+++ src/TopMostArea/mainArea.js
@@ -1,13 +1,27 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 import './mainArea.css'
 class mainArea extends Component{
     constructor(props){
         super(props)
         this.state={
- 
+            searchKeyWord:'',
         }
     }
-
+handleSearch=(evt)=>{
+    console.log(evt.target.value)
+   
+    this.setState({
+        searchKeyWord:evt.target.value,
+    })
+}
+submitSearch=()=>{
+    console.log(this.state.searchKeyWord)
+    axios.get('http://content.guardianapis.com/tags?api-key=fc9dc684-1029-4847-830a-b19cc6f67841')
+    .then(response=>{
+      console.log(response.data)
+    })
+}
     render(){
         return(
         <header>
@@ -16,7 +30,12 @@ class mainArea extends Component{
                 <p class='subtitle'>By Anurag, using Guardian API</p>
             </div>
             <div class='search'>
-                <input></input>
+                <label htmlFor="search" >Search your topics</label><br/><br/>
+                <div class='search_Bar'>
+                    <input type="text" placeholder="Search.." name="search" onChange={this.handleSearch}/>
+                    <button type="submit" onClick={this.submitSearch}><i class="fa fa-search"></i></button>
+                </div>
+
             </div>
         </header>
         )
